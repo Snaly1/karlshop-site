64884b512cfb853b9a85143d81e3d57c1f7ef6a6
Karl <paulbinder.2007@gmail.com>
2026-05-12 12:01:05 +0200

    fix: CSP allow inline handlers

--- src/server.js
+++ src/server.js
@@ -78,7 +78,8 @@ app.use(helmet({
   contentSecurityPolicy: {
     directives: {
       defaultSrc: ["'self'"],
-      scriptSrc:  ["'self'", "'unsafe-inline'"],
+      scriptSrc:  ["'self'", "'unsafe-inline'", "'unsafe-hashes'"],
+      scriptSrcAttr: ["'unsafe-inline'"],
       styleSrc:   ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com'],
       fontSrc:    ["'self'", 'https://fonts.gstatic.com'],
       imgSrc:     ["'self'", 'data:'],
